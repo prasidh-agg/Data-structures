0c99fa329770a6ef0c303d740fab9ec1943ff23b
Prasidh Aggarwal <prasaggarwal@deloitte.com>
2021-05-14 13:37:17 +0530

    Initial Commit Prasidh

---
+++ Java Arrays/src/Array1.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Array1 {
+  int[] numbers = { 1, 2, 3, 4, 5 };
+
+  public static void main(String[] args) {
+    Array1 array1 = new Array1();
+    int[] numbers = { 1, 2, 3, 4, 5 };
+    System.out.println(Arrays.toString(numbers));
+  }
+}
