bf53590eb43efcc887a56cefcfa8e4aef4774803
Prasidh Aggarwal <prasaggarwal@deloitte.com>
2021-05-17 09:53:24 +0530

    Best Seatrch Insert Position Code

--- Java Arrays/src/SearchInsertPosition.java
+++ Java Arrays/src/SearchInsertPosition.java
@@ -2,7 +2,6 @@ public class SearchInsertPosition {
   public static int getInsertPosition(int[] nums, int target) {
     int arrayEnd = nums.length - 1;
     int center;
-    int res = -1;
     int arrayBeg = 0;
     while (arrayBeg <= arrayEnd) {
       center = (arrayEnd + arrayBeg) / 2;
@@ -11,18 +10,16 @@ public class SearchInsertPosition {
       }
       if (target > nums[center]) {
         arrayBeg = center + 1;
-        res = center + 1;
       } else {
         arrayEnd = center - 1;
-        res = center;
       }
     }
-    return res;
+    return arrayBeg;
   }
 
   public static void main(String[] args) {
-    int[] numbers = { 1, 3, 5, 6 };
-    int insertPosition = SearchInsertPosition.getInsertPosition(numbers, 2);
+    int[] numbers = { 1 };
+    int insertPosition = SearchInsertPosition.getInsertPosition(numbers, 0);
     System.out.println("The Insert Position for the number is : " + insertPosition);
   }
 }
